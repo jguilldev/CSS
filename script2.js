1b4c079d0d46cf5e534fc75ce6a53e48d277bf04
jguilldev <168439499+jguilldev@users.noreply.github.com>
2024-10-27 14:55:38 +0000

    envio flex grow column y row, ademas, flex shrink column, pendiente revisar shrink row no funciona

--- script2.js
+++ script2.js
@@ -72,3 +72,59 @@ if (selectFlexGrow && flexGrowItem1 && flexGrowItem2 && flexGrowItem3) {
 
 
     // fin script grow
+
+
+    // inicio script grow row(filas)
+    const selectFlexGrowRow = document.getElementById('flexGrowRowExample');
+    const flexGrowRowItem1 = document.querySelector('.flex-grow-row-item1');
+    const flexGrowRowItem2 = document.querySelector('.flex-grow-row-item2');
+    const flexGrowRowItem3 = document.querySelector('.flex-grow-row-item3');
+
+    selectFlexGrowRow.addEventListener('change', (event) => {
+      const values = event.target.value.split(', ');
+      flexGrowRowItem1.style.flexGrow = values[0];
+      flexGrowRowItem2.style.flexGrow = values[1];
+      flexGrowRowItem3.style.flexGrow = values[2];
+    });
+    // fin script grow row(filas)
+
+
+
+    // inicio script flex shrink en filas
+    const selectFlexShrink = document.getElementById('flexShrinkExample');
+    const flexShrinkItem1 = document.querySelector('.flex-shrink-item1');
+    const flexShrinkItem2 = document.querySelector('.flex-shrink-item2');
+    const flexShrinkItem3 = document.querySelector('.flex-shrink-item3');
+        const flexShrinkContainer = document.getElementById('flexShrinkContainer');
+
+    function applyFlexShrink(values) {
+      flexShrinkItem1.style.flexShrink = values[0];
+      flexShrinkItem2.style.flexShrink = values[1];
+      flexShrinkItem3.style.flexShrink = values[2];
+    }
+
+    selectFlexShrink.addEventListener('change', (event) => {
+      const values = event.target.value.split(',');
+      applyFlexShrink(values);
+    });
+
+    // final script flex shrink en filas
+
+    // inicio de script flex shrink en filas
+    const selectFlexShrinkRow = document.getElementById('flexShrinkRowExample');
+    const flexShrinkRowItem1 = document.querySelector('.flex-shrink-row-item1');
+    const flexShrinkRowItem2 = document.querySelector('.flex-shrink-row-item2');
+    const flexShrinkRowItem3 = document.querySelector('.flex-shrink-row-item3');
+
+    function applyFlexShrinkRow(values) {
+      flexShrinkRowItem1.style.flexShrink = parseInt(values[0]);
+      flexShrinkRowItem2.style.flexShrink = parseInt(values[1]);
+      flexShrinkRowItem3.style.flexShrink = parseInt(values[2]);
+    }
+
+    selectFlexShrinkRow.addEventListener('change', (event) => {
+      const values = event.target.value.split(',');
+      applyFlexShrinkRow(values);
+    });
+    
+    // fin  de script flex shrink en filas
